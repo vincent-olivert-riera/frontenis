72562d789228b57294d2674143d653a057adc1b2
Vicente Olivert Riera <vincent.olivert.riera@gmail.com>
2021-08-21 17:35:04 +0200

    Remove unnecessary stuff from frontend app

--- frontenis/frontend/admin.py
+++
@@ -1,3 +0,0 @@
-from django.contrib import admin  # pylint: disable=unused-import
-
-# Register your models here.
--- frontenis/frontend/models.py
+++
@@ -1,3 +0,0 @@
-from django.db import models  # pylint: disable=unused-import
-
-# Create your models here.
